a5fda071acf98bbdb7c693423e083d6dbcd95119
ima-ocon <ima_ocon@yahoo.com>
2016-03-12 22:45:57 +0800

    Sorted based on arrival time
    Sorted the processes based on arrival time. The processes can also be
    deleted once their arrival time is over

--- Process.hpp
+++ Process.hpp
@@ -10,5 +10,15 @@ struct Process {
   int index;
   bool isDone;
 
-  Process(int a, int b, int p, int i) : arrivalTime(a), burstTime(b), priority(p), isDone(false), index(i) {}
+  Process(int b, int a, int p, int i) : arrivalTime(a), burstTime(b), priority(p), isDone(false), index(i) {}
+
+  bool operator < (const Process& process) const {
+    return (arrivalTime < process.arrivalTime);
+  }
 };
+
+/*struct earliest_arrival_time {
+  inline bool operator() (const Process& struct1, const Process& struct2){
+    return (struct1.arrivalTime < struct2.arrivalTime);
+  }
+};*/
--- priority.cpp
+++ priority.cpp
@@ -16,15 +16,28 @@ int main() {
   //array where algorithms sorted by priority
   //another array where algorithms sorted by arrival time
 
-/*  unusedProcesses[0] = new Process(20, 0, 1, 1);
-  unusedProcesses[1] = new Process(10, 0, 5, 2);
-  unusedProcesses[2] = new Process(15, 0, 4, 3);
-  unusedProcesses[3] = new Process(3, 5, 0, 4);
-  unusedProcesses[4] = new Process(4, 7, 2, 5);*/
-
-/*
+  unusedProcesses.push_back(Process(20, 0, 1, 1));
+  unusedProcesses.push_back(Process(10, 0, 5, 2));
+  unusedProcesses.push_back(Process(15, 0, 4, 3));
+  unusedProcesses.push_back(Process(3, 5, 0, 4));
+  unusedProcesses.push_back(Process(4, 7, 2, 5));
   //time so far, which process, CPU time
 
+  sort(unusedProcesses.begin(), unusedProcesses.end());
+
+  int x = 0;
+  while (x < unusedProcesses.size()) {
+    if (unusedProcesses.at(x).arrivalTime == 0) {
+      cout << "zero";
+      unusedProcesses.erase(unusedProcesses.begin() + x);
+    } else {
+      x++;
+    }
+  }
+
+  for (int x = 0; x < unusedProcesses.size(); x++) {
+    cout << unusedProcesses.at(x).arrivalTime << endl;
+  }
 
   //look for least priority, only if not yet done and arrivalTime <= currentTime
   //keep priority num in mind
@@ -33,7 +46,7 @@ int main() {
   //if yes, set aside then
   //if done, set to -1...?
 
-  int firstPriority = 0;
+/*  int firstPriority = 0;
   int priorityNum = priority[0];
 
   int incomingNum = 0;
