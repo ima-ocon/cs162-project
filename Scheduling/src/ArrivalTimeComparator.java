2523e9970a802dc21d9ea0d09c5537cd3d1d7fd1
Ima Ocon <ima_ocon@yahoo.com>
2016-03-13 22:40:06 +0800

    Accounted for FCFS
    if two processes have the same burst times

--- Scheduling/src/ArrivalTimeComparator.java
+++ Scheduling/src/ArrivalTimeComparator.java
@@ -3,8 +3,8 @@ import java.util.Comparator;
 class ArrivalTimeComparator implements Comparator<Process> {
   @Override
   public int compare(Process p1, Process p2) {
-	  if (p1.arrival == p2.arrival)
-		  return p1.burst - p2.burst;
-	  return p1.arrival - p2.arrival;
+    if (p1.arrival == p2.arrival)
+      return p1.id - p2.id;
+    return p1.arrival - p2.arrival;
   }
 }
